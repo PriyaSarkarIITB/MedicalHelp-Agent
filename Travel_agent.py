272e00c47cc358c2b7604e9fe7cf4e7bd7e6e3d9
Anurag Mohanty <109300269+Anurag2244@users.noreply.github.com>
2025-02-19 19:12:50 +0530

    Update Travel_agent.py
    Changed the backstory prompts of the agents

--- Travel_agent.py
+++ Travel_agent.py
@@ -12,32 +12,59 @@ os.environ["OPENAI_MODEL_NAME"] = 'gpt-4o'
 
 llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0.7, model = 'gpt-4o')
 
-travel_Itinerary_agent = Agent(
-    role = "Travel Route Agent",
-    goal = "It fetches data from the input and makes a route plan for the journey",
-    backstory = "Have expertise in planning packages in different routes across globe"
-                "It can give any package with routes according to the destination in the efficient way according to budget"
-                "seamlessly integrates input, web searches, and delivers the best possible way"
-                "Tries to give appropiate and latest condition about the routes"
-                "uses statiscal model and machine learning"
-                "Gives suggestion according to the budget",
+Travel_Destination_Agent = Agent(
+    role = "Specialized in giving current condition of the destination",
+    goal = "To provide detailed information about current situation of the destination",
+    backstory = "Research on weather plans of the destination for the selected dates"
+                "It identifies best time to travel the specific destination"
+                "Provides local culture, traditions at that particular day"
+                "Also provides information to complete legal works for travelling abroad"
+                "Provides any festival or concert currently going on or going to be happened"
+                "Maps out local transport system"
+                "It provides the optimal distance between the spots in the destination",
     verbose=True,
     allow_delegation=True,
     llm=llm,
     tools = [scrape_tool, search_tool]
 )
 
-travel_package_agent = Agent(
-    role = "Travel Package Agent",
-    goal = "Decides the package after the planning the route",
-    backstory = "Equipped with a deep understanding of developing travel package"
-                "It gives the package according to the budget, route, maximum number of days and number of peoples"
-                "It have deep understanding on managing budget alongwith route and number of peoples to make the travel efficient and comfortable "
-                "continously checks about routes and gives latest updates with alternatives packages according to budget"
-                "tries to get report from different package based on peoples and other factors"
-                "the most best way to get package and regularly stay update for new problems",
+Travel_Route_Planner_Agent = Agent(
+    role = "Gives optimized travel routes for each day",
+    goal = "To create time efficient cost-effective hassle free enjoyable travel plan for travelers",
+    backstory = "Suggest the multiple route option for each day"
+                "Identifies the best transport mode to reach the place for that day to save the time"
+                "Calculates perday travel cost including meal and tickets for spots"
+                "Provides real time traffic situation to avoid delays"
+                "Balances activities and rest periods for each day",
     verbose=True,
     allow_delegation=True,
     llm=llm,
     tools = [scrape_tool, search_tool]
+)
+
+Activity_Recomendation_Agent = Agent(
+    role = "Suggest best activities to perform for that day",
+    goal = "To offer best dynamic and safe experience of the day",
+    backstory = "Suggest activities according to user's interest"
+                "Recommends best landmarks, with high traffic indication"
+                "Checks opening of the activities for the specific dates"
+                "Provides activities cost estimation according to the user's budget"
+                "It will suggest best dynamic and safe activities present in that place"
+                "Suggest best places to take photos",
+    verbose=True,
+    allow_delegation=True,
+    llm=llm,
+    tools = [scrape_tool, search_tool]
+)
+
+Booking_Research_Agent = Agent(
+    role = "Gives the best booking option according to the budget",
+    goal = "To provide hassle free and best travel bookings in a efficient and budget friendly way ",
+    backstory = "Suggest best flight and train according to the budget"
+                "Compares the price between the hotels and transport system and suggest top choices"
+                "Suggest the best local transport option for daily travelling",
+    verbose=True,
+    allow_delegation=True,
+    llm=llm,
+    # tools = [scrape_tool, search_tool]
 )
