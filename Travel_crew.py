bfbdad362d960559d154b944eefec125449884e7
Anurag Mohanty <109300269+Anurag2244@users.noreply.github.com>
2025-02-19 19:15:32 +0530

    Update Travel_crew.py
    Changed the names of the crew because names of the agents and tasks are also changed.

--- Travel_crew.py
+++ Travel_crew.py
@@ -1,16 +1,14 @@
 from crewai import Crew, Process
-from Travel_agent import travel_Itinerary_agent, travel_package_agent 
-from Travel_tasks import travel_Itinerary_task, travel_package_task
+from Travel_agent import Travel_Destination_Agent, Travel_Route_Planner_Agent, Activity_Recomendation_Agent, Booking_Research_Agent
+from Travel_tasks import Travel_Destination_Task, Travel_Route_Planner_Task, Activity_Recomendation_Task, Booking_Research_Task
 from langchain_openai import ChatOpenAI
 import os
 
 manager_llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0.7, model = 'gpt-4o')
 
 travel_agent_crew = Crew(
-    agents = [travel_Itinerary_agent,
-              travel_package_agent],
-    tasks = [travel_Itinerary_task,
-             travel_package_task],
+    agents = [Travel_Destination_Agent, Travel_Route_Planner_Agent, Activity_Recomendation_Agent, Booking_Research_Agent],
+    tasks = [Travel_Destination_Task, Travel_Route_Planner_Task, Activity_Recomendation_Task, Booking_Research_Task],
 
     manager_llm=manager_llm,
     process=Process.hierarchical,
@@ -19,7 +17,7 @@ travel_agent_crew = Crew(
 
 travel_agents_input = {
     'destination':'california',
-    'budget':'400000',
+    'budgets':'400000',
     'max_days':'10',
     'peoples':'5'
 }
